4765785bee072000effb643f653a8bd38c6dd392
Ethan Wright <ewright362@gmail.com>
2017-12-08 14:46:25 -0700

    current

--- src/db/component_sssp.c
+++ src/db/component_sssp.c
@@ -1,8 +1,17 @@
 #include "graph.h"
+#include <stdio.h>
 
 
 /* Place the code for your Dijkstra implementation in this file */
-
+attribute_t 
+find_int_attr(attribute_t a){
+  for(attribute_t attribute = a; attribute != NULL; attribute = attribute->next){
+    if(attribute->bt == 4){
+      return attribute;
+    }
+  }
+  return NULL;
+}
 
 int
 component_sssp(
@@ -12,14 +21,16 @@ component_sssp(
         int *n,
         int *total_weight,
         vertexid_t **path)
-{
-	
+{	
+
 	/*
 	 * Figure out which attribute in the component edges schema you will
 	 * use for your weight function
 	 */
-
-
+//    attribute_t result = find_int_attr(c->se->attrlist);
+    printf("Int value: \n");
+//    printf("%s\n",result->name);
+    printf("Ending now\n");
 
 	/*
 	 * Execute Dijkstra on the attribute you found for the specified
