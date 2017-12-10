23b3036668b6970ac7bc0202eaa42f985b112554
Ethan Wright <ewright362@gmail.com>
2017-12-09 17:20:41 -0700

    getting ready to start dijkstra

--- src/db/component_sssp.c
+++ src/db/component_sssp.c
@@ -23,6 +23,15 @@ set_file_desc(component_t c, vertexid_t v1){
       printf("Open vertex file failed\n");
       return;
   }
+  /* Open the edge file */
+  memset(s, 0, BUFSIZE);
+  sprintf(s, "%s/%d/%d/e", grdbdir, gno, cno);
+  printf("cli_graph_tuple: open edge file %s\n", s);
+  c->efd = open(s, O_RDWR | O_CREAT, 0644);
+  if (c->efd < 0) {
+	  printf("Find edge ids failed\n");
+	  return;
+  }
 }
 
 attribute_t 
@@ -101,6 +110,26 @@ load_vertices(component_t c, vertexid_t *vertices){
     free(buffer);
 }
 
+
+int
+get_weight(component_t c, vertexid_t v1, vertexid_t v2, char* attr_name){
+  struct edge e;
+  edge_t new_edge;
+  edge_init(&e);
+  edge_set_vertices(&e, v1, v2);
+
+  new_edge = component_find_edge_by_ids(c, &e);
+
+  if(new_edge == NULL){
+    printf("v1 is: %llu v2 is: %llu \n", v1, v2);
+    return 100;
+  }
+
+ int offset = tuple_get_offset(new_edge->tuple, attr_name);
+  int weight = tuple_get_int(new_edge->tuple->buf + offset);
+  return weight;
+}
+
 int
 component_sssp(
         component_t c,
@@ -110,19 +139,31 @@ component_sssp(
         int *total_weight,
         vertexid_t **path)
 {
+
   /* Set Vertex File Desc */
   set_file_desc(c, v1);
 
-  /* Get total vert */
+  /* Get total vertices */
   int total = get_vertices_total(c);
   printf("TOTAL IS : %i\n",total);
 
   vertexid_t *vertices;
   vertices = malloc(total * sizeof(vertexid_t));
   load_vertices(c, vertices);
+
   for(int i = 0; i < total; i++){
     printf("vertex id is: %llu\n", vertices[i]);
-}
+  }
+
+  attribute_t result = find_int_attr(c->se->attrlist);
+  char* attr_name = result->name;
+  printf("%s\n",attr_name);
+
+  vertexid_t first = vertices[0];
+  vertexid_t second = vertices[2];
+  int weight = get_weight(c, first, second, attr_name);
+  printf("Weight of first edge is: %i\n", weight);
+
 
 
      /*
@@ -130,15 +171,26 @@ component_sssp(
 	 * use for your weight function
 	 */
 
-  attribute_t result = find_int_attr(c->se->attrlist);
-  char* attr_name = result->name;
-  printf("%s\n",attr_name);
+
 
 	/*
 	 * Execute Dijkstra on the attribute you found for the specified
 	 * component
 	 */
 
+  vertexid_t start = v1;
+  vertexid_t end = v2;
+
+  if(start != vertices[0]){
+    printf("Start must be first vertex\n");
+    return -1;
+  }else if(start == end){
+    printf("They are the same... efficient path I guess.\n");
+    return -1;
+  }else{
+    // Code for running
+
+  }
 
 
 	/* Change this as needed */
